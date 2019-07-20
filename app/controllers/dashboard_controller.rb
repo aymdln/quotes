7fb684b567230b79966821dd07748c33a7e8b974
Aymeric DHALLUIN <aymeric.dhalluin@gmail.com>
2019-07-20 13:09:43 +0200

    Dashboard update

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,35 @@
 class DashboardController < ApplicationController
   def index
+    
+    links
+
+    if current_user.third_party.manufacturer?
+      @quotes = current_user.third_party.relations_as_manufacturer.map do |relation|
+        relation.quotes
+      end
+    end
   end
 
+  private
+
+  def links
+    @links = [
+      {
+        name: "Home",
+        icon: "fa fa-home",
+        path: "#",
+      },
+      {
+        name: "Clients",
+        icon: "fa fa-user",
+        path: "#",
+      },
+      {
+        name: "Devis",
+        icon: "fa fa-file-alt",
+        path: "#",
+      },
+    ]
+  end
+  
 end
